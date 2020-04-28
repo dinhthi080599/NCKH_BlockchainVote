e351b6afc156e4b56ee82b751d03d553921b9252
HoangTrungg <hoangtrung1198@gmail.com>
2020-04-28 23:38:22 +0700

    UpdateInfo

--- CSharpchainWebAPI/Controllers/ProfileController.cs
+++ CSharpchainWebAPI/Controllers/ProfileController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CSharpchainWebAPI.Models;
+using System.Globalization;
+
 namespace CSharpchainWebAPI.Controllers
 {
     public class ProfileController : BaseController
@@ -23,6 +25,25 @@ namespace CSharpchainWebAPI.Controllers
             ViewBag.account = account;
             ViewBag.Title = "Hồ sơ cá nhân";
             return View();
+        }
+
+        // update info
+        [HttpPost]
+        public bool EditInfor()
+        {
+            DateTime dt1;
+            var Name = Request["Name"];
+            var Birthday = Request["Birthday"];
+            var Address = Request["Address"];
+            var Email = Request["Email"];
+            var Gender = Request["Gender"];
+            var Phone = Request["Phone"];
+            var Id = long.Parse(Request["id"]);
+            if (Birthday.Length != 0)
+                dt1 = DateTime.ParseExact(Birthday, "dd/mm/yyyy", CultureInfo.InvariantCulture);
+            Account account = new Account();
+            if (account.EditAccountInfo(Id, Name, Gender, Birthday.ToString(), Phone, Email, Address)) return true;
+            else return false;
         }
     }
 }
